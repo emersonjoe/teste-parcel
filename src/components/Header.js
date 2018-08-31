b57184085b8042463400bd76973bb43bfe0c43a4
Emerson Oliveira dos Santos <emerson.santos@asbpm.com.br>
2018-08-31 12:05:58 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Header = () => (
+    <header>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="/">
+                    Era Um Logo Aqui=x
+                </a>
+            </div>
+        </nav>
+    </header>
+)
+
+export default Header
